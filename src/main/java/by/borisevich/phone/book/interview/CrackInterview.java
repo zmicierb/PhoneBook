f49d34eee2674e80f691802891f8c5bba626caf9
Dmitry Borisevich <dborisevich@marathonbet.ru>
2018-05-16 14:57:44 +0300

    Crack Interview: Chapter1

---
+++ src/main/java/by/borisevich/phone/book/interview/CrackInterview.java
@@ -0,0 +1,13 @@
+package by.borisevich.phone.book.interview;
+
+
+import by.borisevich.phone.book.interview.chapter1.Utils;
+
+
+public class CrackInterview
+{
+    public static void main(String[] args)
+    {
+        System.out.println(Utils.isUniqueChars("qwertqyuio"));
+    }
+}
